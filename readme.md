81b54c5552d73f0535d7186f7a77431281c1e2eb
Manish Gill <contact@manishgill.com>
2020-09-12 23:27:25 +0200

    Added todo

--- readme.md
+++ readme.md
@@ -1,3 +1,11 @@
 # Rsh - A Rusty Shell
 
 A simple shell that I am writing just for fun. Don't use it for serious business.
+
+## Todo
+
+- Nice Prompt
+- Builtins
+- Env
+- No Zombies
+- Trap Signals
--- src/main.rs
+++ src/main.rs
@@ -2,32 +2,31 @@ use std::io::{self, Write};
 use std::process::Command;
 
 fn main() {
-    let stdin = io::stdin();    // TODO: "concurrent reads of Stdin must be executed with care" ?? need to know more
+    let stdin = io::stdin();
     println!("Welcome to rsh!");
-    print!("rsh > ");
-    io::stdout().flush().unwrap();
     loop {
+        print!("rsh> ");
+        io::stdout().flush().unwrap();
         let mut command_str = String::new();
         match stdin.read_line(&mut command_str) {
             Ok(_) => {
                 command_str = command_str.trim().to_string();
                 interpret(&command_str);
-                print!("rsh> ");
-                io::stdout().flush().unwrap();
             }
             Err(error) => println!("command not found = {}", error),
         }
     }
 }
 
+/**
+ * How can we write tests for this?
+ */
 fn interpret(command_str: &String) {
-    // print!("We got a command to interpret = {}", command)
-    // can we split up the command string into an array/list of sub-strings?
     if command_str.len() == 0 {
         return
     }
     let command_split : Vec<&str> = command_str.split(' ').collect();
-    // println!("{:?}", command_split);
+    
     let process = command_split[0];
     
     // TODO: Can we directly work with syscalls somehow?
